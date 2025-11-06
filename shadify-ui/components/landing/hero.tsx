4421570657cc2ad279d64541732b55392e7f90cd
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-07 03:52:20 +0530

    Enhance hero section visuals and animations for better engagement

--- shadify-ui/components/landing/hero.tsx
+++ shadify-ui/components/landing/hero.tsx
@@ -13,8 +13,8 @@ import { TbBrandFramerMotion } from "react-icons/tb";
 import { TbBrandReactNative } from "react-icons/tb";
 import Login from '../auth_services/login';
 import { CodeBlock } from "@/components/landing/code-block";
-
-
+import { motion } from "motion/react"
+import AnnoncementBadge from "./AnnoncementBadge";
 interface Action {
   id: string;
   label: string;
@@ -78,16 +78,26 @@ export default function Login2() {
 
 export function HeroSection() {
   return (
-    <div className="w-full h-auto mx-auto container lg:px-4 py-14 lg:py-16 flex flex-col items-center justify-center text-center gap-6 relative">
+    <div className="w-full h-auto mx-auto container py-14 lg:py-16 flex flex-col items-center justify-center text-center gap-6 relative ">
       <div className="w-full h-auto px-2 lg:px-4 py-6">
-        <span className="mx-auto w-auto lg:w-[310px] md:w-[310px] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] font-sans text-sm px-6 py-px rounded-full bg-neutral-100 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200 flex gap-2 items-center justify-center">New Components: let's expolore all <IoIosArrowRoundForward className="text-lg"/></span> 
+        <AnnoncementBadge />
         <div className="mt-8 text-center flex-col gap-4">
           <h1 className="font-sans font-bold text-3xl lg:text-6xl text-neutral-800 dark:text-neutral-200">A Modern Component & Template Library for Fast, Beautiful Frontends.</h1>
-          <p className="px-0 lg:w-2/3 mx-auto py-4 font-sans font-medium text-md text-neutral-600 dark:text-neutral-400">Fully customizable React components and production-ready templates — built with clean code, modern design principles, and performance at the core.</p>
+          <p className="px-0 lg:w-2/3 mx-auto py-4 font-sans font-medium text-md text-neutral-600 dark:text-neutral-400 ">Fully customizable React components and production-ready templates — built with clean code, modern design principles, and performance at the core.</p>
         </div>
         <div className="flex flex-wrap justify-center gap-4 pt-8">
-          <button className="font-sans font-medium px-10 py-2 rounded-md bg-neutral-100 text-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Explore Templets</button>
-          <button className="font-sans font-medium px-10 py-2 rounded-md bg-neutral-900 text-neutral-200 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Components Documentation</button>
+          <motion.button
+            transition={{ duration: 0.28 , ease: "easeInOut"}}
+            whileHover={{ y: -3 }}
+            whileTap={{ y: -4 }}
+          className="relative overflow-hidden cursor-pointer font-sans font-medium px-10 py-2 rounded-md bg-neutral-100 bg-gradient-to-t from-[#f5f5f5] to-[#d4d4d4] dark:text-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Explore Templets  </motion.button>
+          <motion.button
+              transition={{ duration: 0.28 , ease: "easeInOut"}}
+              whileHover={{ y: -3}}
+              whileTap={{ y: -4 }}
+          className=" relative cursor-pointer font-sans font-medium px-10 py-2 rounded-md bg-gradient-to-t from-[#262626] to-[#525252] text-neutral-200 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Components Documentation
+          </motion.button>
+          
         </div>
       </div>
       <div className='flex flex-wrap justify-evenly lg:justify-center py-0 gap-4 lg:gap-6 poppins-medium text-neutral-600'>
@@ -98,25 +108,27 @@ export function HeroSection() {
           <span className='flex gap-1 justify-center items-end text-[14px] lg:text-[15px] font-sans font-bold '> <span className='text-3xl lg:text-4xl'><RiRemixRunLine/></span>Remix</span>
           <span className='flex gap-1 justify-center items-end text-[14px] lg:text-[15px] font-sans font-bold '> <span className='text-3xl lg:text-4xl'><SiShadcnui/></span>Shadcn</span>
         </div>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-1 w-full justify-center py-4 relative ">
-          <div className="lg:w-[400px] w-full relative top-10"><AICardGeneration/></div>
-          <div className="flex flex-col gap-4 lg:w-auto w-full"><CheckoutInteraction /><Alert04/></div>
-          <div className="hidden lg:flex w-[400px] relative top-12"><Profile04 /></div>
+        <div className="w-full h-auto grid grid-cols-1 md:grid-cols-2  lg:grid-cols-4 pt-10 gap-2 lg:gap-4 justify-evenly items-center">
+          <div className="relative h-[300px] rounded-xl bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex items-center justify-center overflow-hidden">
+            <div className=" z-10 absolute inset-0 h-full w-full bg-white dark:bg-neutral-900 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)] bg-[size:10px_10px]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)] dark:bg-[radial-gradient(circle_500px_at_50%_200px,#171717,transparent)]"></div></div>
+            
+          </div>
+          <div className="relative overflow-hidden h-[300px] rounded-xl bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+            <div className="absolute inset-0 h-full w-full bg-white dark:bg-neutral-900 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)] bg-[size:10px_10px]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)] dark:bg-[radial-gradient(circle_500px_at_50%_200px,#171717,transparent)]"></div></div>
+          </div>
+          <div className="relative overflow-hidden h-[300px] rounded-xl bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+            <div className="absolute inset-0 h-full w-full bg-white dark:bg-neutral-900 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)] bg-[size:10px_10px]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)] dark:bg-[radial-gradient(circle_500px_at_50%_200px,#171717,transparent)]"></div></div>
+          </div>
+          <div className="relative overflow-hidden h-[300px] rounded-xl bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+            <div className="absolute inset-0 h-full w-full bg-white dark:bg-neutral-900 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)] bg-[size:10px_10px]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)] dark:bg-[radial-gradient(circle_500px_at_50%_200px,#171717,transparent)]"></div></div>
+          </div>
         </div> 
-        {/* <div className="w-full lg:h-[676px] h-[350px] aspect-video overflow-hidden mt-20 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] rounded-md"> 
-          <video
-            className="w-full rounded-md object-cover h-full"
-            src={"https://res.cloudinary.com/dou5rypdf/video/upload/v1761979295/showcase_-_Made_with_Clipchamp_y0czxl.mp4"}
-            loop
-            autoPlay
-          ></video>
-        </div> */}
 
         <br /><br />
         <div className="pt-10">
           <h1 className="font-sans font-bold text-3xl lg:text-6xl text-neutral-800 dark:text-neutral-200">Easy to Integrate, Build faster ship faster and Deploy smoothly</h1>
           <div className="w-full h-auto grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 justify-between py-10 items-center">
-            <div className='bg-neutral-50 dark:bg-neutral-950 rounded-lg md:h-[450px] lg:h-[500px] w-[100%] overflow-auto shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] mt-20'>
+            <div className='bg-neutral-50 dark:bg-neutral-950 rounded-lg h-[400px] md:h-[450px] lg:h-[500px] w-[100%] overflow-auto shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] mt-20'>
               <CodeBlock 
                 language="jsx"
                 filename="DummyComponent.jsx"
