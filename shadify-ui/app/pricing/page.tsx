359edb8793d4b130e9dca0b9a54b7fca8629cad6
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-14 03:37:14 +0530

    Enhance pricing page layout, plan components, and responsiveness

--- shadify-ui/app/pricing/page.tsx
+++ shadify-ui/app/pricing/page.tsx
@@ -2,6 +2,9 @@ import  Header  from '@/components/landing/header'
 import React from 'react'
 import { MdDone } from "react-icons/md";
 import { BsStars } from "react-icons/bs";
+import FAQ from './FAQ';
+import { StripedPattern } from "@/components/magicui/striped-pattern"
+import StripedPatternDemo from './striped-pattern';
 function Pricing() {
   return (
     <div className='bg-[#f4f4f5] dark:bg-neutral-950 h-full'>
@@ -15,8 +18,8 @@ function Pricing() {
             <p className="text-sm font-sans font-bold text-neutral-700 dark:text-neutral-300">PRICING PLAN</p>
             <span className="h-14 absolute right-4 -top-4 border border-dashed  border-neutral-300 dark:border-neutral-800"></span>          
           </div>
-          <div className="flex items-center items-center px-10 lg:px-28">
-            <h1 className="lg:text-6xl md:text-5xl text-4xl font-sans font-bold text-center space-y-4">Simply choose the pricing plan that <br /> <span className="ml-4 px-2 text-orange-500 bg-neutral-200 dark:bg-neutral-900 rounded-lg shadow-[inset_-12px_-8px_40px_#46464620]"> fits you best.</span></h1>
+          <div className="flex items-center px-10 lg:px-28">
+            <h1 className="lg:text-6xl md:text-5xl text-4xl font-sans font-bold text-center space-y-4">Simply choose the pricing plan that <br /> <span className="ml-4 px-2 text-transparent bg-clip-text bg-gradient-to-r from-[#bf953f] to-[#aa771c] rounded-lg"> fits you best.</span></h1>
           </div>
         </div>
         <div className='flex gap-8 flex-wrap justify-center items-end pt-10'>
@@ -53,8 +56,6 @@ function Pricing() {
 
             <div className='relative px-6 py-6 lg:w-[400px] md:w-auto w-full h-auto border-2 dark:ring-neutral-900 rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] bg-neutral-50 dark:bg-neutral-900'>
 
-                <div className=" absolute h-full w-full bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:10px_10px] bg-gradient-to-r from-orange-500 to-yellow-300  [mask-image:radial-gradient(ellipse_80%_100%_at_100%_100%,#000_50%,transparent_100%)]"></div>
-
 
 
                 <div className='absolute -top-4 left-[50%] translate-x-[-50%] flex items-center justify-center'><p className='flex gap-2 pramiun_pricing_btn px-8 py-[2px] text-sm rounded-full border-2 text-neutral-800 premium font-sans font-medium text-center'><span><BsStars /></span>Popular</p></div>
@@ -69,7 +70,9 @@ function Pricing() {
 Access all templates, advanced themes, and regular updates building high-quality apps.</p>
                 </div>
                 <div className='relative w-full h-full'>
-                  <div className='bg-white dark:bg-neutral-800 rounded-lg mt-4 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full h-auto pb-3'>
+
+                  <div className='relative overflow-hidden bg-white dark:bg-neutral-800 rounded-lg mt-4 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full h-auto pb-3'>
+                      <StripedPattern direction="left" className="mask-l-from-50% mask-l-to-80% mask-t-from-20% to-90% " />
                     <div className='pt-4 px-3'>
                       <h1 className='font-sans text-sm text-neutral-600'>What's included:</h1>
                       <div className='px-3 py-5'>
@@ -84,7 +87,7 @@ Access all templates, advanced themes, and regular updates building high-quality
                       </div>
                     </div>
                     <div className='w-full flex justify-center items-center px-6'>
-                        <button className='w-full py-2 rounded-xl pramiun_pricing_btn cursor-pointer text-neutral-800 font-medium text-neutral-200 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]'>Get premium</button>
+                        <button className='z-20 w-full py-2 rounded-xl pramiun_pricing_btn cursor-pointer text-neutral-800 font-medium shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]'>Get premium</button>
                     </div>
 
                 </div>
@@ -125,14 +128,17 @@ Access all templates, advanced themes, and regular updates building high-quality
             
         </div>
       </div>
+        <div className="relative w-[100%] h-auto border-t border-b border-dashed border-neutral-300 dark:border-neutral-800 z-10">
+          <StripedPattern direction="left" className="mask-l-from-70% mask-l-to-100% mask-r-from-70% mask-r-to-100%" />
+        <div className='relative container h-[100%] !important border-l border-r border-dashed z-50 !important bg-neutral-100 dark:bg-neutral-950 pb-5'>
+          <FAQ />
+        </div>
+    </div>
+      <br /> <br /> <br /> 
     </div>
   )
 }
 
 
 export default Pricing
-
-
-
-
 
