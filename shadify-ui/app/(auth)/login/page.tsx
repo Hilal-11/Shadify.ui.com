d6f6566b9eb062da16249a02a5c6fe17700c7858
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-24 23:39:11 +0530

    authentication

--- shadify-ui/app/(auth)/login/page.tsx
+++ shadify-ui/app/(auth)/login/page.tsx
@@ -9,8 +9,7 @@ import { IoLogoGithub } from "react-icons/io5";
 import { cn } from "@/lib/utils";
 import { z } from 'zod'
 import Header from "@/components/landing/header"
-
-const loginSchema = z.object({
+const signupSchema = z.object({
   username: z.preprocess(
     (v) => typeof v === 'string' ? v.trim() : v,
     z.string().min(3, "username is required")
@@ -25,7 +24,6 @@ const loginSchema = z.object({
 const  Login = () => {
 
   const [errors, setErrors] = useState<Record<string, string>>({})
-
   const [loginFormData , setLoginFormData] = useState({
     username: "",
     email: "",
@@ -41,7 +39,7 @@ const  Login = () => {
     setErrors({});
 
 
-    const parse = loginSchema.safeParse(loginFormData);
+    const parse = signupSchema.safeParse(loginFormData);
     if(!parse.success) {
       const fieldErrors: Record<string, string> = {};
       for (const issue of parse.error.issues) {
@@ -66,48 +64,54 @@ const  Login = () => {
   return (
     <div className="w-full h-auto">
       <Header />
-    <div className="flex flex-col py-2 w-full h-screen justify-center items-center px-4">
-      <div
-              className={cn(
-                "absolute inset-0",
-                "[background-size:40px_40px]",
-                "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-                "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
-              )}
-            />
-    <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-    
+    <div className="flex flex-col w-full h-screen relative">
+      
+      <div className="w-svw h-[100%] z-30 bg-white dark:bg-neutral-900
+      shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] px-4 py-4">
 
+        <form onSubmit={formSubmitHandler} className="">
+            <h1 className="text-4xl font-bold ">Login</h1>
+            <p className="font-sans font-medium text-sm pl-1.5 text-neutral-700 dark:text-neutral-300 pb-8 py-1">Login to your account if you already have an account.</p>
 
-      <div className="z-30 bg-white border dark:bg-neutral-900 w-full lg:w-[450px] md:w-[450px] h-auto p-6
-      shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] rounded-lg py-4 px-4 flex justify-center items-center">
-
-        <form onSubmit={formSubmitHandler} className="w-full lg:w-[400px] md:w-[400px]">
-            <h1 className="text-4xl font-bold text-center">Login</h1>
-            <p className="text-sm text-center text-muted-foreground pb-8 py-2">Create your account</p>
-
-            <div className="mt-4">
-                <Label className="text-sm font-medium pb-1" htmlFor="email">Email</Label>
-                <Input onChange={onChangeFormHandler} value={loginFormData.email} id="email" name="email" type="email" placeholder="johndoe@example.com"/>
+            <div className="mt-3">
+                <Label className="text-[15px] font-sans font-medium pb-1 px-1" htmlFor="username">Username</Label>
+                <Input onChange={onChangeFormHandler} value={loginFormData.username} className="py-5" id="username" name="username" type="text" placeholder="Enter username:- "/>
+                <div className='text-red-600 font-sans font-bold px-1 text-[12px] py-1 '>{errors.username}</div>
+            </div>
+            <div className="mt-3">
+                <Label className="text-[15px] font-sans font-medium pb-1 px-1" htmlFor="email">Email</Label>
+                <Input onChange={onChangeFormHandler} value={loginFormData.email} className="py-5" id="email" name="email" type="email" placeholder="Enter email ID:- "/>
                 <div className='text-red-600 font-sans font-bold px-1 text-[12px] py-1 '>{errors.email}</div>
             </div>
-            <div className="mt-4">
-                <Label className="text-sm font-medium pb-1" htmlFor="password">Password</Label>
-                <Input onChange={onChangeFormHandler} value={loginFormData.password} id="password" name="password" type="password" placeholder="••••••••"/>
+            <div className="mt-3">
+                <Label className="text-[15px] font-sans font-medium pb-1 px-1" htmlFor="password">Password</Label>
+                <Input onChange={onChangeFormHandler} value={loginFormData.password} className="py-5" id="password" name="password" type="password" placeholder="Enter Password:-"/>
                 <div className='text-red-600 font-sans font-bold px-1 text-[12px] py-1 '>{errors.password}</div>
             </div>
+             <div className="mt-1 flex justify-between w-full px-2 text-sm font-medium text-neutral-600">
+                <div className="flex gap-2 items-center justify-center">
+                  <input type="checkbox" id="rememberMe" name="rememberMe" />
+                  <p className="dark:text-neutral-300">Remember Me</p>
+                </div>
+                <div><p className="dark:text-neutral-300">Forget password?</p></div>
+            </div>
+            <div className="mt-8">
+                <Button className="w-full cursor-pointer font-sans font-medium px-10 py-6 rounded-md bg-gradient-to-t from-[#262626] to-[#525252] text-neutral-200 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] text-md" type="submit">Login</Button>
+            </div>
 
-            <div className="mt-6">
-                <Button className="w-full py-2 rounded-md cursor-pointer" type="submit">Signup</Button>
+            <div className="mt-6 mb-6 flex justify-between items-center gap-3 px-4">
+              <div className="border border-gray-300 w-full h-px"></div>
+              <span className="font-medium font-sans text-sm">OR</span>
+              <div className="border border-gray-300 w-full h-px"></div>
             </div>
             <div className="mt-3 flex gap-1 w-full">
-                <Button className="bg-tranaparent dark:bg-neutral-100 text-neutral-800 hover:text-white py-2 rounded-md cursor-pointer w-1/2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]" type="submit"><span><FaGoogle  /></span>Google</Button>
-                <Button className="bg-tranaparent dark:bg-neutral-100 text-neutral-800 hover:text-white py-2 rounded-md cursor-pointer w-1/2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]" type="submit"><span><IoLogoGithub  /></span>GitHub</Button>
+                <Button className="w-full cursor-pointer font-sans font-medium px-10 py-6 rounded-md bg-gradient-to-t from-[#f5f5f5] to-[#d4d4d4] text-neutral-800 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] text-md" type="submit"><span className="text-2xl"><FaGoogle  /></span>Google</Button>
+            </div>
 
+            <div className="mt-4 text-center text-sm 0 mx-auto">
+                <p >Don not have an account? <Link href="/signup" className="text-neutral-500">signup</Link></p>
             </div>
-            <div className="mt-4 text-center text-sm">
-                <p>Don not have an account? <Link href="/signup" className="text-neutral-500">signup</Link></p>
-            </div>
+
         </form>
       </div>
     </div>
--- shadify-ui/app/(auth)/signup/page.tsx
+++ shadify-ui/app/(auth)/signup/page.tsx
@@ -24,7 +24,6 @@ const signupSchema = z.object({
 const  Signup = () => {
 
   const [errors, setErrors] = useState<Record<string, string>>({})
-
   const [signupFormData , setSignupFormData] = useState({
     username: "",
     email: "",
@@ -65,54 +64,54 @@ const  Signup = () => {
   return (
     <div className="w-full h-auto">
       <Header />
-    <div className="flex flex-col py-2 w-full h-screen justify-center items-center px-4">
+    <div className="flex flex-col w-full h-screen relative">
       
-      <div
-              className={cn(
-                "absolute inset-0",
-                "[background-size:40px_40px]",
-                "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-                "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
-              )}
-            />
-    <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-    
+      <div className="w-svw h-[100%] z-30 bg-white dark:bg-neutral-900
+      shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] px-4 py-4">
 
+        <form onSubmit={formSubmitHandler} className="">
+            <h1 className="text-4xl font-bold ">Signup</h1>
+            <p className="font-sans font-medium text-sm pl-1.5 text-neutral-700 dark:text-neutral-300 pb-8 py-1">Create your account and get start building.</p>
 
-      <div className="z-30 bg-white border dark:bg-neutral-900 w-full lg:w-[450px] md:w-[450px] h-auto p-6
-      shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] rounded-lg py-4 px-4 flex justify-center items-center">
-
-        <form onSubmit={formSubmitHandler} className="w-full lg:w-[400px] md:w-[400px]">
-            <h1 className="text-4xl font-bold text-center">Signup</h1>
-            <p className="text-sm text-center text-muted-foreground pb-8 py-2">Create your account</p>
-
-            <div className="mt-4">
-                <Label className="text-sm font-medium pb-1" htmlFor="username">Username</Label>
-                <Input onChange={onChangeFormHandler} value={signupFormData.username} id="username" name="username" type="text" placeholder="johndoe"/>
+            <div className="mt-3">
+                <Label className="text-[15px] font-sans font-medium pb-1 px-1" htmlFor="username">Username</Label>
+                <Input onChange={onChangeFormHandler} value={signupFormData.username} className="py-5" id="username" name="username" type="text" placeholder="Enter username:- "/>
                 <div className='text-red-600 font-sans font-bold px-1 text-[12px] py-1 '>{errors.username}</div>
             </div>
-            <div className="mt-4">
-                <Label className="text-sm font-medium pb-1" htmlFor="email">Email</Label>
-                <Input onChange={onChangeFormHandler} value={signupFormData.email} id="email" name="email" type="email" placeholder="johndoe@example.com"/>
+            <div className="mt-3">
+                <Label className="text-[15px] font-sans font-medium pb-1 px-1" htmlFor="email">Email</Label>
+                <Input onChange={onChangeFormHandler} value={signupFormData.email} className="py-5" id="email" name="email" type="email" placeholder="Enter email ID:- "/>
                 <div className='text-red-600 font-sans font-bold px-1 text-[12px] py-1 '>{errors.email}</div>
             </div>
-            <div className="mt-4">
-                <Label className="text-sm font-medium pb-1" htmlFor="password">Password</Label>
-                <Input onChange={onChangeFormHandler} value={signupFormData.password} id="password" name="password" type="password" placeholder="••••••••"/>
+            <div className="mt-3">
+                <Label className="text-[15px] font-sans font-medium pb-1 px-1" htmlFor="password">Password</Label>
+                <Input onChange={onChangeFormHandler} value={signupFormData.password} className="py-5" id="password" name="password" type="password" placeholder="Enter Password:-"/>
                 <div className='text-red-600 font-sans font-bold px-1 text-[12px] py-1 '>{errors.password}</div>
             </div>
+             <div className="mt-1 flex justify-between w-full px-2 text-sm font-medium text-neutral-600">
+                <div className="flex gap-2 items-center justify-center">
+                  <input type="checkbox" id="rememberMe" name="rememberMe" />
+                  <p className="dark:text-neutral-300">Remember Me</p>
+                </div>
+                <div><p className="dark:text-neutral-300">Forget password?</p></div>
+            </div>
+            <div className="mt-8">
+                <Button className="w-full cursor-pointer font-sans font-medium px-10 py-6 rounded-md bg-gradient-to-t from-[#262626] to-[#525252] text-neutral-200 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] text-md" type="submit">Signup</Button>
+            </div>
 
-            <div className="mt-6">
-                <Button className="w-full py-2 rounded-md cursor-pointer" type="submit">Signup</Button>
+            <div className="mt-6 mb-6 flex justify-between items-center gap-3 px-4">
+              <div className="border border-gray-300 w-full h-px"></div>
+              <span className="font-medium font-sans text-sm">OR</span>
+              <div className="border border-gray-300 w-full h-px"></div>
             </div>
             <div className="mt-3 flex gap-1 w-full">
-                <Button className="bg-tranaparent dark:bg-neutral-100 text-neutral-800 hover:text-white py-2 rounded-md cursor-pointer w-1/2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]" type="submit"><span><FaGoogle  /></span>Google</Button>
-                <Button className="bg-tranaparent dark:bg-neutral-100 text-neutral-800 hover:text-white py-2 rounded-md cursor-pointer w-1/2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]" type="submit"><span><IoLogoGithub  /></span>GitHub</Button>
+                <Button className="w-full cursor-pointer font-sans font-medium px-10 py-6 rounded-md bg-gradient-to-t from-[#f5f5f5] to-[#d4d4d4] text-neutral-800 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] text-md" type="submit"><span className="text-2xl"><FaGoogle  /></span>Google</Button>
+            </div>
 
+            <div className="mt-4 text-center text-sm 0 mx-auto">
+                <p >Don not have an account? <Link href="/login" className="text-neutral-500">login</Link></p>
             </div>
-            <div className="mt-4 text-center text-sm">
-                <p>Don not have an account? <Link href="/login" className="text-neutral-500">login</Link></p>
-            </div>
+
         </form>
       </div>
     </div>
