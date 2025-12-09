abc6914a60528a1cc7bcaba207cdb7d004443a4a
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-12-09 17:49:13 +0530

    Enhance templates page with improved layout and components

--- shadify-ui/app/templates/page.tsx
+++ shadify-ui/app/templates/page.tsx
@@ -76,7 +76,6 @@ function Templates(){
                             placeholder="Search the creative work"
                             className="w-full h-[58px] rounded-full outline-none pl-8"
                         />
-                        <button className="flex items-center gap-1 h-[40px] lg:h-[50px] whitespace-nowrap bg-neutral-950 text-center text-neutral-900 px-4 lg:px-10 rounded-full inter-square shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] bg-[linear-gradient(90deg,_#F6D5F7_0%,_#FBE9D7_100%)]"><span className="lg:text-xl text-sm"><BsStars /></span>GET TEMPLATE</button>
                     </div>
                 </div>
                 <div className="flex flex-col justify-center items-center pt-4">
