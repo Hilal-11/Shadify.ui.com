351afc9d4455031713a80a2774114b653013a18d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-12-15 15:07:02 +0530

    Update landing header navigation and styling

--- shadify-ui/components/landing/header.tsx
+++ shadify-ui/components/landing/header.tsx
@@ -21,7 +21,6 @@ import {
 import { services } from "@/config/servicesConfig"
 import { FaChevronDown } from "react-icons/fa6";
 import { FaChevronUp } from "react-icons/fa";
-import { Link2OffIcon } from "lucide-react";
 function Header() {
   const [isAuthenticate , setIsAuthenticate] = useState(false)
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
