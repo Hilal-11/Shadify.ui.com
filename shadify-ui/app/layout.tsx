1c27e2bc667d779a48d341062de0943a77334eb2
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-12-20 05:54:24 +0530

    dashboard

--- shadify-ui/app/layout.tsx
+++ shadify-ui/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Header from "./header";
 
 import Providers from "./provider";
 const geistSans = Geist({
@@ -30,6 +30,7 @@ export default function RootLayout({
         className={`font-sans ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
+          <Header />
           {children}
         </Providers>
       </body>
