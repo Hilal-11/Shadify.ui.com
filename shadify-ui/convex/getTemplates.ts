89821dea654478e055344e9389f376d7486d7f60
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-12-22 16:46:53 +0530

    Refactor template fetching logic

--- shadify-ui/convex/getTemplates.ts
+++ shadify-ui/convex/getTemplates.ts
@@ -16,3 +16,18 @@ export const getTemplateById = query({
         return await ctx.db.get(args.id)
     }
 })
+
+export const getDesignKits = query({
+    args: {},
+    handler: async (ctx) => {
+        const designKits = await ctx.db.query('designKits').collect();
+        return designKits
+    }
+})
+
+export const getDesignKitById = query({
+    args: { id: v.id("designKits") },
+    handler: async(ctx , args) => {
+        return await ctx.db.get(args.id)
+    }
+})
