d03cefa78fa8704ac24d32f0ffe3d8195abc80d5
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-06 01:48:09 +0530

    done

--- shadify-ui/app/header.tsx
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-import HeaderClient from "./headerClient";
-
-export default async function Header() {
-  const cookieStore = await cookies(); // ✅ AWAIT
-  const token = cookieStore.get("token")?.value;
-
-  return <HeaderClient isLoggedIn={!!token} />;
-}
--- shadify-ui/registry/index.ts
+++
@@ -1,6 +0,0 @@
-import { component } from "./registry-components";
-import { lib } from "./registry-lib";
-import type { Registry } from "./schema";
-import { block } from "./registry-blocks";
-
-export const registry: Registry = [...component, ...lib, ...block];
