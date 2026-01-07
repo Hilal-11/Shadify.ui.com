dccb750e56673be277d25ce97dbaea727ecd3d8e
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-08 00:53:03 +0530

    Enhance landing hero section layout and interactions

--- lokalhost_io/components/landing/hero.tsx
+++ lokalhost_io/components/landing/hero.tsx
@@ -8,6 +8,13 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 import { servicesShowCaseConfig } from '../../config/servicesConfig';
 import { useEffect, useState } from "react";
 import { ThemeProvider , useTheme } from "next-themes"
+import { heroServiceContent } from "@/config/hero_section_service_config"
+import Link from "next/link";
+import { HiOutlineExternalLink } from "react-icons/hi";
+import { ContainerTextFlip } from "./animate_hero_tags"
+import DesignsShowcase from "./LandingComponents/DesignsShowcase";
+import TemplatesShowcase from "./LandingComponents/TemplatesShowcase";
+
 export function HeroSection() {
 
   const { theme } = useTheme();
@@ -19,109 +26,79 @@ export function HeroSection() {
   }, [serviceId])
 
 
-
-{/* <img
-  src={theme === 'dark' ? '/image-dark.png' : '/image-light.png'}
-  alt=""
-/> */}
-
   return (
-    <div className="w-full h-auto mx-auto md:container lg:container py-8 lg:py-16 flex flex-col items-center justify-center text-center gap-6 relative">
+    <div className="w-full h-auto mx-auto md:container lg:container py-8 lg:py-16 flex flex-col items-center justify-center text-center gap-6">
+      <motion.span
+       initial={{opacity: 0 , y: -100}}
+       animate={{opacity: 1 , y: 0}}
+       transition={{ delay: 0.2, duration: 0.98 , ease: 'easeInOut'}}
+       className="absolute top-0 left-0 lg:-top-28 lg:left-40 w-[60px] lg:w-[170px] h-[300px] lg:h-[600px] bg-neutral-300 dark:bg-neutral-800 blur-2xl rounded-b-full -rotate-45 "></motion.span>
+       
       <div className="w-full h-auto px-0 lg:px-4 py-6">
         <AnnoncementBadge />
         <div className="mt-8 text-center flex-col gap-4">
-          <h1 className="font-sans font-bold text-3xl lg:text-6xl text-neutral-800 dark:text-neutral-200">A Modern Component & Template Library for Fast, Beautiful Frontends.</h1>
-          <p className="px-0 lg:w-2/3 mx-auto py-4 font-sans font-medium text-md text-neutral-600 dark:text-neutral-400 ">Fully customizable React components and production-ready templates — built with clean code, modern design principles, and performance at the core.</p>
+          <h1 className="font-sans font-bold text-3xl lg:text-5xl text-neutral-800 dark:text-neutral-200">A Complete Ecosystem for Designing, Building, and Scaling Modern Web & Mobile Applications.</h1>
+          <ContainerTextFlip/>
+          <p className="px-0 lg:w-2/3 mx-auto py-4 font-sans font-medium text-md text-neutral-600 dark:text-neutral-400 ">A unified ecosystem built for modern product development, covering web and mobile UI, design systems, authentication, motion, and creative assets — all optimized for production use</p>
         </div>
         <div className="flex flex-wrap justify-center gap-4 pt-8">
           <motion.button
             transition={{ duration: 0.28 , ease: "easeInOut"}}
             whileHover={{ y: -3 }}
             whileTap={{ y: -4 }}
-          className="relative overflow-hidden cursor-pointer font-sans font-medium px-10 py-2 rounded-md bg-neutral-100 bg-gradient-to-t from-[#f5f5f5] to-[#d4d4d4] dark:text-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Explore Templets  </motion.button>
+          className="border-t-[2px] border-l-[2px] border-r-[2px] border-neutral-50 relative overflow-hidden cursor-pointer font-sans font-medium px-10 py-2 rounded-md bg-neutral-100 bg-gradient-to-t from-[#f5f5f5] to-[#d4d4d4] dark:text-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Explore Templets  </motion.button>
           <motion.button
               transition={{ duration: 0.28 , ease: "easeInOut"}}
               whileHover={{ y: -3}}
               whileTap={{ y: -4 }}
-          className="relative cursor-pointer font-sans font-medium px-10 py-2 rounded-md bg-gradient-to-t from-[#262626] to-[#525252] text-neutral-200 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Components Documentation
+          className="border-t-[2px] border-l-[2px] border-r-[2px] border-neutral-950 relative cursor-pointer font-sans font-medium px-10 py-2 rounded-md bg-gradient-to-t from-[#262626] to-[#525252] text-neutral-200 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Components Documentation
           </motion.button>
           
         </div>
       </div>
 {/*  */}
-        <div className="relative h-auto w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 border-t gap-2 md:gap-0 lg:gap-0 border-b border-dashed px-px">
-          {/* verticel border-dashed lines */}
-          <span className="absolute h-[800px] border-l border-dashed left-0 top-0"></span>
-          <span className="absolute h-[800px] border-r border-dashed right-0 top-0"></span>
+        <div className="relative h-auto w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 border-t gap-4 md:gap-0 lg:gap-0 border-b border-dashed px-px">
 
           {/* corners design */}
-          <span className="hidden lg:block absolute h-[160px] w-[160px] rounded-full border border-dashed -top-20 -left-20"></span>
-          <span className="hidden lg:block absolute h-[160px] w-[160px] rounded-full border border-dashed -top-20 -right-20"></span>
-
-          <div className="relative h-[400px] bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex items-center justify-center overflow-hidden w-full border-r border-dashed">
-            <div className="dark:hidden w-full h-[100px] absolute -top-12 bg-gradient-to-r from-[hsla(312,77%,86%,1)] to-[hsla(220,61%,79%,1)] rounded-b-full blur-lg"></div>
-            <div>
-              <Image 
-                width={400}
-                height={650}
-                src={theme === 'dark' ? "https://res.cloudinary.com/dou5rypdf/image/upload/v1767732075/image_comp_dark_xwdxwb.png" : "https://res.cloudinary.com/dou5rypdf/image/upload/v1767728958/image_comp_vzgzhv.png"}
-                alt=""
-              />
-            </div>
-            <div className="absolute bottom-2 px-5">
-              <h1 className="font-sans font-medium text-[16px] text-left">Web Components</h1>
-              <p className="text-[12px] font-sans font-medium text-neutral-400 text-left">Explore the production ready components for React and Next.Js apps.</p>
-            </div>
-          </div>
-
-
-          <div className="relative overflow-hidden h-[400px] bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full border-r border-dashed">
-            <div className="dark:hidden w-full h-[100px] absolute -top-12 bg-gradient-to-r from-[hsla(277,79%,84%,1)] to-[hsla(204,95%,77%,1)] rounded-b-full blur-lg"></div>
-            <div>
-              <Image 
-                width={400}
-                height={650}
-                src={theme === 'dark' ? "https://res.cloudinary.com/dou5rypdf/image/upload/v1767730724/image_temp_dark2_bkl2f6.png" : "https://res.cloudinary.com/dou5rypdf/image/upload/v1767729440/image_templat_r9b3hs.png"}
-                alt=""
-              />
-            </div>
-          </div>
-
-
-
-
-          <div className="relative overflow-hidden h-[400px] bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full border-r border-dashed">
-            <div className="dark:hidden w-full h-[100px] absolute -top-12 bg-gradient-to-r from-[hsla(212,35%,58%,1)] to-[hsla(218,32%,80%,1)] rounded-b-full blur-lg"></div>
-            <div>
-               <Image 
-                width={400}
-                height={650}
-                src={theme === 'dark' ? "https://res.cloudinary.com/dou5rypdf/image/upload/v1767732075/image_mob_dark_t4wia5.png" : "https://res.cloudinary.com/dou5rypdf/image/upload/v1767729440/image_mob_w4f6v3.png"}
-                alt=""
-              />
-            </div>
-          </div>
-
-
-          
-          <div className="relative overflow-hidden h-[400px] bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full border-r border-dashed ">
-            <div className="dark:hidden w-full h-[100px] absolute -top-12 bg-gradient-to-r from-[hsla(233,100%,90%,1)] to-[hsla(0,0%,89%,1)] rounded-b-full blur-lg"></div>
-            <div>
-              <Image 
-                width={400}
-                height={650}
-                src={theme === 'dark' ? "https://res.cloudinary.com/dou5rypdf/image/upload/v1767732075/image_mob_dark_t4wia5.png" : "https://res.cloudinary.com/dou5rypdf/image/upload/v1767729440/image_design_xo7czm.png"}
-                alt=""
-              />
-            </div>
-          </div>
-
-          <div className="relative overflow-hidden h-[400px] bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full border-r border-dashed border-t"></div>
-
-          <div className="relative overflow-hidden h-[400px] bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full border-r border-dashed border-t"></div>
-
+          <span className="absolute h-[160px] w-[160px] rounded-full border border-dashed -top-20 -left-20"></span>
+          <span className="absolute h-[160px] w-[160px] rounded-full border border-dashed -top-20 -right-20"></span>
+          {
+            heroServiceContent.map((service) => (
+              <Link key={service.id} href={service.service_link}>
+              <div  className="cursor-pointer group relative h-[400px] bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex items-center justify-center overflow-hidden w-full border border-dashed">
+                <div className="dark:hidden z-20 w-full h-[100px] absolute -top-12 bg-gradient-to-r from-[hsla(312,77%,86%,1)] to-[hsla(220,61%,79%,1)] rounded-b-full blur-lg"></div>
+                <div className="relative group-hover:scale-110 transition duration-400">
+                  <Image 
+                    width={400}
+                    height={650}
+                    className="relative scale-105"
+                    src={theme === 'dark' ? `${service.service_dark_image}` : `${service.service_light_image}`}
+                    alt=""
+                  />
+                </div>
+                <div className="absolute bottom-3 px-5 flex flex-col gap-2">
+                  <div className="">
+                    <h1 className="font-sans font-medium text-[17px] text-left text-neutral-700 dark:text-neutral-200">{service.sercice_name}</h1>
+                    <p className="text-[13px] font-sans font-medium text-neutral-500 dark:text-neutral-300 text-left">{service.service_disc}</p>
+                  </div>
+                  <div className="flex justify-center gap-2">
+                    <motion.button
+                     transition={{ duration: 0.38 , ease: "easeInOut"}}
+                     whileHover={{ y: -2 }}
+                     whileTap={{ y: -4 }}
+                     className="cursor-pointer border-t border-l border-r border-neutral-800 rounded-sm py-px whitespace-nowrap px-4 font-sans font-medium text-sm text-neutral-200 dark:text-neutral-200 bg-gradient-to-t from-[#262626] to-[#525252] shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">{service.service_buttons[0].button_name}</motion.button>
+                    <motion.button 
+                      transition={{ duration: 0.38 , ease: "easeInOut"}}
+                      whileHover={{ y: -2 }}
+                      whileTap={{ y: -4 }}
+                      className="cursor-pointer flex gap-1 items-center justify-center py-px w-full font-sans font-medium text-sm text-neutral-700 dark:text-neutral-200">{service.service_buttons[1].button_name} <span className="text-xs text-neutral-700 dark:text-neutral-200"><HiOutlineExternalLink /></span></motion.button>
+                  </div>
+                </div>
+              </div>
+              </Link>
+            ))
+          }
           <div className="md:col-span-2 lg:col-span-2 relative overflow-hidden h-[400px] bg-neutral-100 dark:bg-neutral-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full border-r border-dashed border-t"></div>
-
         </div> 
 
         <br /><br />
@@ -130,7 +107,7 @@ export function HeroSection() {
           
           <div className="lg:mt-20 w-full h-auto grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 justify-between lg:py-10 items-between">
               <div className="w-full flex justify-end lg:hidden">
-                <span className="text-3xl relative top-6 bg-neutral-200 shadow-md border rounded-full dark:bg-neutral-800 flex justify-center items-center"><IoIosArrowRoundForward /></span>
+                <span className="text-3xl relative top-3 bg-neutral-200 shadow-md border rounded-full dark:bg-neutral-800 flex justify-center items-center"><IoIosArrowRoundForward /></span>
               </div>
               <div className="flex lg:flex-col justify-between lg:h-[525px] flex-nowrap overflow-auto gap-2 pl-2">
 
@@ -163,55 +140,40 @@ export function HeroSection() {
                {/* for components */}
                 { 
                   service.id === 1 && <div className="w-full h-auto gap-2 grid lg:grid-cols-3 grid-cols-2">
-                    <div className="h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
-                    <div className="h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
-                    <div className="h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
-                    <div className="h-[260px] lg:col-span-2 rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
-                    <div className="h-[260px] lg:col-span-1 col-span-2 rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] lg:col-span-2 rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] lg:col-span-1 col-span-2 rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
                   </div>
                 }
+
+                {/* form mobile elememnts */}
+                { 
+                  service.id === 3 && <div className="w-full h-auto gap-2 grid lg:grid-cols-3 grid-cols-2">
+                    <div className="h-[120px] lg:h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] lg:col-span-2 rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                    <div className="h-[120px] lg:h-[260px] lg:col-span-1 col-span-2 rounded-sm shadow-sm border hover:bg-neutral-100 transition duration-300 hover:dark:bg-neutral-950"></div>
+                  </div>
+                }
+
                {/* for templates view */}
-               <div className="grid grid-cols-2 gap-6 rotate-25 justify-end items-end pr-28">
-                <motion.div
-                    initial={{ opacity: 0, y: -280 }}   // start from TOP
-                    whileInView={{ opacity: 1, y: -150 }} // move DOWN smoothly
-                    viewport={{ once: false }} 
-                    transition={{
-                      duration: 2.6,
-                      ease: 'easeInOut',
-                    }}
-
-                 className="grid grid-cols-1 w-full h-full gap-6 justify-between relative">
-                {
-                  service.id === 2 && service?.templates_list_1?.map((template) => (
-                    <div key={template?.id} className={`flex justify-center items-center text-xl font-medium font-sans h-[200px] lg:h-[300px] shadow-sm border rounded-sm bg-neutral-100 dark:bg-neutral-800`}>
-                      <h1>{template.id}</h1>
-                    </div>
-                  ))
+               {
+                service.id === 2 && <div>
+                  <TemplatesShowcase />
+                </div>
+               }
+              {/*  fro designs */}
+              { 
+                  service.id === 4 && <div className="w-full h-full">
+                    <DesignsShowcase />
+                  </div>
                 }
-               </motion.div>
-               <motion.div
-                  initial={{ opacity: 0, y: -280 }}   // start from TOP
-                    whileInView={{ opacity: 1, y: -150 }} // move DOWN smoothly
-                    viewport={{ once: false }} 
-                    transition={{
-                      duration: 1.6,
-                      ease: 'easeInOut',
-                    }}
-
-                className="grid grid-cols-1 w-full h-full gap-6 justify-between relative">
-                {
-                  service.id === 2 && service?.templates_list_2?.map((template) => (
-                    <div key={template?.id} className={`flex justify-center items-center text-xl font-medium font-sans h-[200px] lg:h-[300px] shadow-sm border rounded-sm bg-neutral-100 dark:bg-neutral-800`}>
-                      <h1>{template.id}</h1>
-                    </div>
-                  ))
-                }
-               </motion.div>
-               </div>
-               {/* for compo */}
+              
+ 
             </div>
-
           </div>
         </div>
     </div>
