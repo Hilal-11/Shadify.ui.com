c500afc0b3c9088dc3927f9fe03da21398f8813f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-08 00:53:03 +0530

    Update announcement badge content and styling

--- lokalhost_io/components/landing/AnnoncementBadge.tsx
+++ lokalhost_io/components/landing/AnnoncementBadge.tsx
@@ -11,7 +11,7 @@ function AnnoncementBadge() {
         )}
       >
         <AnimatedShinyText className="inline-flex items-center justify-center font-sans font-medium px-4 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400 text-sm">
-          <span>✨ Introducing Lokalhost Apps</span>
+          <span>✨ Open for Client Work</span>
           <IoIosArrowRoundForward className="ml-1 size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
         </AnimatedShinyText>
       </div>
