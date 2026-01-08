98d14b389e91bf038c11e5e7faf79217ec250f4e
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-08 20:55:19 +0530

    Refine landing page FAQ section

--- lokalhost_io/components/landing/LandingComponents/FAQ.tsx
+++ lokalhost_io/components/landing/LandingComponents/FAQ.tsx
@@ -147,7 +147,7 @@ export const FinalMainFAQ = () => {
     return (
         <div className='w-full h-full pt-8 lg:pt-20'>
             <div className='w-full mx-auto py-20 pb-5 flex flex-col items-center justify-center'>
-                <h1 className='text-center font-sans font-bold text-2xl lg:text-6xl text-neutral-800 dark:text-neutral-200 pb-2'>Frequently Asked Questions about Lokalhost.io</h1>
+                <h1 className='text-center font-sans font-bold text-2xl lg:text-4xl text-neutral-800 dark:text-neutral-200 pb-2'>Frequently Asked Questions about Lokalhost.io</h1>
                 <p className='text-center font-sans font-medium text-sm text-neutral-700 dark:text-neutral-300'>Find answers to common questions about our services, features, and how Lokalhost.io can benefit you.</p>
             </div>
             <div className='grid md:grid-cols-2 lg:grid-cols-2 grid-cols-1 pt-16 px-6 gap-6 lg:gap-16'>
