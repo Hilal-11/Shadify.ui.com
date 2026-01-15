b6463c68ec83c265d7de4b3e5ccdc16ac3d9dd90
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-16 04:52:11 +0530

    Update feedback API route

--- lokalhost_io/app/api/feedback/route.ts
+++ lokalhost_io/app/api/feedback/route.ts
@@ -1,32 +1,33 @@
-import { NextResponse , NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import Feedback from "@/lib/models/feedback";
 import dbConnect from "@/lib/dbConnect";
 
-await dbConnect()
 export const POST = async (request: Request) => {
+  try {
+    await dbConnect();
+
     const body = await request.json();
-    const { feedback_title , feedback_discription } = body
-
-    if(!feedback_title || !feedback_discription) {
-        return new NextResponse(
-            JSON.stringify({message: "Please give the correct feedback"}),{status: 400}
-        )
+    const { feedback_title, feedback_discription } = body;
+    if (!feedback_title || !feedback_discription) {
+      return NextResponse.json(
+        { message: "Please give the correct feedback" },
+        { status: 400 }
+      );
     }
-    try{
-        const feedback = {
-            feedback_title,
-            feedback_discription
-        }
-        const newFeedback = new Feedback(feedback)
-        await newFeedback.save();
 
-        return new NextResponse(
-            JSON.stringify({ message: "Thanks for your Feedback", feed: newFeedback }),{status: 200}
-        )
+    const response = await Feedback.create({
+        feedback_title,
+        feedback_discription
+    });
+    return NextResponse.json(
+      { message: "Thanks for your Feedback", feed: response },
+      { status: 201 }
+    );
 
-    }catch(error:any) {
-        return new NextResponse(
-            JSON.stringify({ message:"Failed to send feedback, some internal issues"+ error.message }),{status: 501}
-        )
-    }
-}
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: "Failed to send feedback", error: error.message },
+      { status: 500 }
+    );
+  }
+};
