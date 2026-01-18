49fa192d63e42ac96fb0840bf50467417ff3b972
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-18 15:35:36 +0530

    Update hero section service configuration

--- lokalhost_io/config/hero_section_service_config.ts
+++ lokalhost_io/config/hero_section_service_config.ts
@@ -1,4 +1,47 @@
 
+
+
+// Features config
+interface IFeatureType {
+    button_name: string,
+    button_link: string
+}
+
+interface IFeatures {
+    feature_id: string
+    feature: string
+    feature_about: string
+    feature_about_2?: string
+    feature_about_3?: string
+    feature_links?: string
+    feature_buttons?: IServiceType[]
+}
+
+export const FEATURES_BENTO_CONFIG: IFeatures[] = [
+    {
+        feature_id: "lokalhost_io",
+        feature: "Lokalhost.io",
+        feature_about: "The lokalhost.io is a modern template market place, creative design ui kits, Component library, pre-built functional components, authentication kits for SASS, Mobile application blocks and mobile apps for cross-platform [React-native and Flutter]. lokalhost.io provides the funational, modern assets for developing a modern web apps and mobile apps.",
+        // rotating circulat tech stack 
+    },
+    {
+        feature_id: "desig-mocro",
+        feature: "Modern UI/UX with Micro-Intractions",
+        feature_about: "Lokalhost.io focus on modern web and mobile technologies, crafting a feuristic web and mobile experiances.",
+        feature_about_2: "Work with web technologies Next.Js | React.Js |  | Motion | Gsap | Three.Js",
+        feature_about_3: "lokalhost.io with cross-patform mobile devs technologies React Native | Expo | Flutter | Motion | Gsap | Animate.Js",
+        feature_buttons: [{ button_name: "Lets start", button_link: "/docs"},{ button_name: "Get Templates Free", button_link: "/templates"}]
+    },
+    {
+        feature_id: "prouction",
+        feature: "Production Ready | Easy to ship",
+        feature_about: "Lokalhost.io focus on modern web and mobile technologies, crafting a feuristic web and mobile experiances.",
+        feature_about_2: "Work with web technologies Next.Js | React.Js |  | Motion | Gsap | Three.Js",
+        feature_about_3: "lokalhost.io with cross-patform mobile devs technologies React Native | Expo | Flutter | Motion | Gsap | Animate.Js",
+    },
+]
+
+
 interface IServiceType {
     button_name: string,
     button_link: string
@@ -86,47 +129,6 @@ export const heroServiceContent: IHeroSectionSevice[] = [
             },
         ]
     },
-    {
-        id: "Authentication",
-        sercice_name: "Authentication Kits",
-        service_disc: "Explore the production ready components for React and Next.Js apps.",
-        service_link: "/authdocs",
-        service_light_image: "https://res.cloudinary.com/dou5rypdf/image/upload/v1767729440/image_design_xo7czm.png",
-        service_dark_image  : "https://res.cloudinary.com/dou5rypdf/image/upload/v1767732075/image_mob_dark_t4wia5.png",
-        service_buttons: [
-            {
-                button_name: "Authentication",
-                button_link: "/authdocs",
-            },
-            {
-                button_name: "Easy to Integrate",
-                button_link: "/authdocs",
-            },
-        ]
-    },
-    {
-        id: "UIPatterns",
-        sercice_name: "UI Patterns",
-        service_disc: "Explore the production ready components for React and Next.Js apps.",
-        service_link: "/bg-patterns",
-        service_light_image: "https://res.cloudinary.com/dou5rypdf/image/upload/v1767728958/image_comp_vzgzhv.png",
-        service_dark_image  : "https://res.cloudinary.com/dou5rypdf/image/upload/v1767732075/image_comp_dark_xwdxwb.png",
-        service_buttons: [
-            {
-                button_name: "UI Patterns",
-                button_link: "/bg-patterns",
-            },
-            {
-                button_name: "Make Pattern",
-                button_link: "/bg-patterns",
-            },
-        ]
-    },
 ]
 
-interface ITemplateList {
-    id: string | number,
-    template_name: string,
-    template_image: string,
-    template_url: string
-}
+
