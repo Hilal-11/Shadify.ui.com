c868340288dbc86ebe812993db9e4267202afdce
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-22 01:04:59 +0530

    Improve designs page layout and content

--- lokalhost_io/app/designs/page.tsx
+++ lokalhost_io/app/designs/page.tsx
@@ -1,9 +1,6 @@
 "use client"
-
 import { useRef } from "react"
-
 import { useRouter } from "next/navigation";
-
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { BsStars } from "react-icons/bs";
@@ -12,6 +9,15 @@ import { IoMdSearch } from "react-icons/io";
 import { Suspense } from 'react'
 import Video from 'next-video';
 import getStarted from '/videos/mobile_ui_kits.mp4';
+import Image from "next/image";
+import TemplateShimmerLoadingUI from '@/components/templateShimmerLoadingUI';
+import { Button } from "@/components/ui/button"
+import { ButtonGroup } from "@/components/ui/button-group"
+import { HiArrowNarrowRight } from "react-icons/hi";
+import HoverExternalIcon from "@/components/landing/MicroComponents/HoverExternalIcon";
+import { InputGroup, InputGroupButton, InputGroupInput } from "@/components/ui/input-group";
+
+
 
 function DegignKits(){
 
@@ -47,36 +53,114 @@ function DegignKits(){
         <div className="relative">
 
             <AnimatedGridPatternDemo />
-            <div className="relative w-full container max-w-[1580px] pt-14 w-full h-auto z-40">
+            <div className="relative w-full container max-w-[1580px] pt-14 h-auto z-40">
                             <AnnoncementBadge aboutBadge={"20 + Premium Templates and Designs by lokalhost.io"}/>
                             <div className="lg:pt-16 pt-8  w-full h-auto mx-auto">
                                 <div className="w-full mx-auto text-center lg:px-14">
                                     <h1 className="font-sans font-bold text-3xl lg:text-5xl text-neutral-800 dark:text-neutral-200">A high-quality collection of templates for React, Next.js, and modern web stacks, designed to ship faster.</h1>
                                     <p className="px-0 lg:w-2/3 mx-auto py-4 font-sans font-medium text-md text-neutral-600 dark:text-neutral-400">Discover a curated library of responsive templates and reusable components built for React, Next.js, and modern web technologies. Whether you're a freelancer, startup, or enterprise team.</p>
                                 </div>
-                               
                             </div>
                              <div className="flex flex-wrap justify-center items-center pt-3 pb-5 gap-3 lg:gap-6">
                                 <button className="border-1 border-orange-400 cursor-pointer px-8 py-[9px] rounded-lg text-sm font-sans font-medium text-neutral-800 bg-gradient-to-r from-[#F6D5F7] to-[#FBE9D7] shadow-sm flex items-center justify-center gap-2"><span><LuFigma /></span> Get Full Design Access</button>
                                 <button className="px-8 py-[10px] cursor-pointer border-t border-l border-r border-neutral-800 rounded-lg whitespace-nowrap font-sans font-medium text-sm text-neutral-200 dark:text-neutral-200 bg-gradient-to-t from-[#262626] to-[#525252] shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">Get All Premium Templates</button>
                             </div>
             
-            <div className="grid grid-cols-2 gap-4 pt-8">
-                <div className="w-full h-auto rounded-xl bg-neutral-100 dark:bg-neutral-900 p-5">
+            <div className="grid grid-cols-1 w-full">
+                <div className="w-full lg:h-[700px] md:h-[700px] overflow-hidden rounded-xl bg-neutral-100 bg-gradient-to-r from-[#F6D5F7] to-[#FBE9D7] p-4">
                     <Suspense fallback={<div>Loading...</div>}>
-                        <Video src={getStarted} />
-                    </Suspense>
-                </div>
-                <div className="w-full h-auto rounded-xl bg-neutral-100 dark:bg-neutral-900 p-5">
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Video src={getStarted} />
+                        <Video src={getStarted} autoPlay/>
                     </Suspense>
                 </div>
             </div>
                            
-            
+            <section className="h-auto pt-4 mx-auto border border-dashed border-neutral-300 dark:border-neutral-700 mt-24">
+                <div className="flex justify-end gap-2 items-center w-full pt-0 pb-4 border-b border-dashed border-neutral-300 dark:border-neutral-700 pr-5">
+                    <div className="flex items-center gap-2 pl-5">
+                        <InputGroup className="flex items-center justify-center">
+                            <InputGroupInput placeholder="Search Template:- " className="hidden lg:flex md:flex"/>
+                            <InputGroupButton variant="secondary" className="bg-transparent flex items-center justify-center"><IoMdSearch className="text-lg mx-auto mr-px"/></InputGroupButton>
+                        </InputGroup>
+                    </div>
+                    <div>
+                        <ButtonGroup>
+                            <Button variant="outline">All</Button>
+                            <Button variant="outline">Free</Button>
+                            <Button variant="outline">Premium</Button>
+                        </ButtonGroup>
+                    </div>
+                </div>     
+                { !designsKits ? (<TemplateShimmerLoadingUI/>) : (
+                    designsKits?.map((design) => (
+                        <div key={design.id} className="flex flex-col gap-4 w-full">
+                        <div className="w-full h-auto flex flex-wrap border-t border-dashed border-neutral-300 dark:border-neutral-700 r">
+                            <div className="xl:w-[30%] lg:w-[40%] md:w-[50%] px-6 border-r border-dashed border-neutral-300 dark:border-neutral-700 py-5 pb-5">
+                                <div className="flex flex-col gap-3">
+                                    <h1 className="font-sans font-bold text-3xl text-neutral-800 dark:text-neutral-200">{design.name}</h1>
+                                    <p className="pt-2 text-sm font-sans font-medium text-neutral-600 dark:text-neutral-400">{design.discription}</p>
+                                </div>
+                                <div className="flex justify-between pt-4">
+                                    <button className="px-3 border border-dashed rounded-sm font-sans font-medium text-sm text-neutral-800 dark:text-neutral-200">{design._creationTime.toLocaleString("en-US")}</button>
+                                    <button className="px-4 py-2 cursor-pointer border-t border-l border-r border-neutral-800 rounded-lg whitespace-nowrap font-sans font-medium text-xs text-neutral-200 dark:text-neutral-200 bg-gradient-to-t from-[#262626] to-[#525252] shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">{design.price === "free" ? `Free` : `${design.price !== "Free"? `${design.price}` : "Free"} `}</button>
+                                </div>
+                                <div className="flex w-full justify-start items-center pt-4">
+                                    <button onClick={() => router.push(`/templatesView/${design._id}`)} className="px-8 py-[6px] cursor-pointer border-t border-l border-r border-neutral-800 rounded-lg whitespace-nowrap font-sans font-medium text-sm text-neutral-200 dark:text-neutral-200 bg-gradient-to-t from-[#262626] to-[#525252] shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] flex gap-2 items-center">View Template <span className="text-lg"><HiArrowNarrowRight /></span> </button>
+                                </div>
+                                
+                            </div>
+                            <div onClick={() => router.push(`/designView/${design._id}`)} className="cursor-pointer xl:w-[70%] lg:w-[60%] md:w-[50%] px-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3 mask-l-from-80% to-100% p-14 relative group transition duration-300">
+                                <div className="z-40 hidden absolute top-1 right-1 group-hover:block transition duration-300">
+                                    <HoverExternalIcon />
+                                </div>                        
+                                                        
+                                <div className="border rounded-lg shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] flex items-center justify-center">
+                                  {design.images?.[0] && (
+                                     <Image 
+                                         src={design.images[0]}
+                                         alt="not load yet"
+                                         width={400}
+                                         height={500}
+                                         className="object-cover h-full rounded-t-md w-full"
+                                     />
+                                     
+                                 )}
+                                </div>
+                                <div className="border rounded-lg shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] flex items-center justify-center">
+                                    {design.images?.[1] && (
+                                     
+                                     <Image 
+                                         src={design.images[1]}
+                                         alt="not load yet"
+                                         width={400}
+                                         height={500}
+                                         className="object-cover h-full rounded-t-md w-fulld"
+                                     />
+                                     
+                                 )}
+                                </div>
+                                <div className="border rounded-lg shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] flex items-center justify-center">
+                                    {design.images?.[2] && (
+                                     
+                                     <Image 
+                                         src={design.images[2]}
+                                         alt="not load yet"
+                                         width={400}
+                                         height={500}
+                                         className="object-cover h-full rounded-t-md w-fulld"
+                                     />
+                                     
+                                 )}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    ))
+                    
+                )}
+            </section>
+
             </div>   
-    
         {/* <Footer /> */}
         </div>
     )
@@ -94,7 +178,7 @@ import AnnoncementBadge from "@/components/landing/AnnoncementBadge";
 import { LuFigma } from "react-icons/lu";
 export function AnimatedGridPatternDemo() {
   return (
-    <div className="pointer-events-none absolute inset-0 z-0 overflow-hidden mask-x-from-80% mask-y-from-98% to-100%">
+    <div className="pointer-events-none absolute inset-0 z-0 mask-x-from-80% mask-y-from-98% to-100%">
     <AnimatedGridPattern
       numSquares={50}
       maxOpacity={0.1}
@@ -102,11 +186,10 @@ export function AnimatedGridPatternDemo() {
       repeatDelay={1}
       className={cn(
         "absolute inset-0",
-        "inset-y-[-30%] h-[200%]"
+        "inset-y-[-30%] h-[900px]"
       )}
     />
   </div>
-
   )
 }
 
