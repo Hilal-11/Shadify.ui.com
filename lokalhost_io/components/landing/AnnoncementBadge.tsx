801103cffa1a102c8c57c56c6c2386a4baebf05e
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-22 01:05:00 +0530

    Improve announcement badge styling and behavior

--- lokalhost_io/components/landing/AnnoncementBadge.tsx
+++ lokalhost_io/components/landing/AnnoncementBadge.tsx
@@ -4,7 +4,7 @@ function AnnoncementBadge({ aboutBadge }: { aboutBange?: string }) {
   return (
     <div className="z-10 flex  items-center justify-center px-5">
       <ShimmerButton className="shadow-2xl">
-      <p className="text-center text-sm flex items-center gap-2 whitespace-nowrap overflow-hidden lg:w-full md:w-full w-[300px]">
+      <p className="text-center text-sm flex items-center gap-2 whitespace-nowrap overflow-hidden lg:w-full md:w-full w-[270px]">
         <span className="flex items-center justify-center bg-neutral-100 text-center font-mono text-[11px] px-2 h-[12px] py-px rounded-full text-neutral-800 ">New</span> {aboutBadge} <span className="pl-1"><HiArrowNarrowRight className="text-xs" /></span>
       </p>
     </ShimmerButton>
