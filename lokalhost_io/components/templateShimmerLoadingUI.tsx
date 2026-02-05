cacba9c7b634a53a71e468870c3d1f4f65f3978b
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-02-05 21:50:33 +0530

    Improve template shimmer loading UI

--- lokalhost_io/components/templateShimmerLoadingUI.tsx
+++ lokalhost_io/components/templateShimmerLoadingUI.tsx
@@ -4,20 +4,20 @@ import { cn } from "@/lib/utils"
 
 function TemplateShimmerLoadingUI() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4 justify-evenly">
-        <div className="h-[350px] rounded-xl bg-neutral-50 dark:bg-neutral-900  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex justify-center items-center"
+    <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-4 justify-evenly p-6">
+        <div className="h-[350px] w-full rounded-xl bg-neutral-50 dark:bg-neutral-900  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex justify-center items-center"
         >
             <Spinner/>
         </div>
-        <div className="h-[350px] rounded-xl bg-neutral-50 dark:bg-neutral-900  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex justify-center items-center"
+        <div className="h-[350px] w-full rounded-xl bg-neutral-50 dark:bg-neutral-900  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex justify-center items-center"
         >
             <Spinner/>
         </div>
-        <div className="h-[350px] rounded-xl bg-neutral-50 dark:bg-neutral-900  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex justify-center items-center"
+        <div className="h-[350px] w-full rounded-xl bg-neutral-50 dark:bg-neutral-900  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex justify-center items-center"
         >
             <Spinner/>
         </div>
-        <div className="h-[350px] rounded-xl bg-neutral-50 dark:bg-neutral-900  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex justify-center items-center"
+        <div className="h-[350px] w-full rounded-xl bg-neutral-50 dark:bg-neutral-900  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex justify-center items-center"
         >
             <Spinner/>
         </div>
